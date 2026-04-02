70e103b32fb3b2228dbdca415d30c6f13439b715
Ademola Afolabi <afolabiademola27@gmail.com>
2026-04-02 13:48:18 +0100

    feat: integrate PrimeVue and related themes for enhanced UI components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from '../router'
 import { initApiInterceptors } from '@/utils/api'
@@ -9,6 +12,11 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
+app.use(PrimeVue, {
+	theme: {
+		preset: Aura
+	}
+})
 app.use(router)
 
 initApiInterceptors(router)
